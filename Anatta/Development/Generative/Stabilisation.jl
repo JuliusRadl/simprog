292618c0980cb45acb755c6ee8c0e35af791b0f9
JuliusRadl <julius.radl@student.hswt.de>
2026-06-27 15:14:35 +0200

    finished 308

--- Anatta/Development/Generative/Stabilisation.jl
+++ Anatta/Development/Generative/Stabilisation.jl
@@ -34,10 +34,15 @@ Create the stabilisation model.
 """
 function stabilisation(;
 	binding_radius = 0.2,			# Radius of attraction between Particles
+	sticky_x = 0,
+	sticky_y = 0
 )
-	extent = (20,20)
+	extent = (20, 20)
 	properties = Dict(
 		:binding_radius => binding_radius,
+		:sticky_x => sticky_x,
+		:sticky_y => sticky_y,
+		:extent => extent
 	)
 
 	world = StandardABM( Particle, ContinuousSpace(extent, spacing=0.5);
@@ -63,7 +68,22 @@ nearby particles, and then wiggles to face in a new random direction.
 function agent_step!( particle, world)
 	# Chance of moving is lower if other particles are nearby:
 	if rand() < (1 / (1 + length(collect(nearby_agents(particle,world,world.binding_radius)))^2))
-		move_agent!(particle, world)
+		# chance of moving is lower if near sticky location
+		# distance between agent pos and sticky pos
+		x = world.sticky_x
+		y = world.sticky_y
+		sticky_pos = (x, y)
+		dist = Agents.euclidean_distance(particle.pos, sticky_pos, world)
+
+		# distance as proportion of maximum distance possible in the space
+		# (distance between the center and a corner, since the space is periodic)
+		max_dist = ((world.extent[1]/2)^2 + (world.extent[2]/2)^2)^0.5
+		
+		# relative distance squared (exponentially stronger effect when closer)
+		proximity = (1 - (dist / max_dist)^2)
+		if rand() > proximity
+			move_agent!(particle, world)
+		end
 	end
 	wiggle!(particle,pi/9)
 end
@@ -77,6 +97,8 @@ Set up a playground for a simple stabilisation process.
 function demo()
 	params = Dict(
 		:binding_radius => 0.05:0.05:1,
+		:sticky_x => 0:1:20,
+		:sticky_y => 0:1:20
 	)
 
 	plotkwargs = (
